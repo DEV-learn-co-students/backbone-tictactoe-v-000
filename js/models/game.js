049316381bca2ee989f6be102849025d75e7056d
stephaniecoleman <sproutwingsandfly@gmail.com>
2015-05-18 15:22:59 -0400

    Finish game specs.

--- js/models/game.js
+++ js/models/game.js
@@ -17,31 +17,95 @@
 				[2,5,8], 
 				[0,4,8],
 				[2,4,6]
-			]
-		}},
+			],
+			endStatus: null
+		}
+	},
+
+		hasMark: function(id){
+			if (this.get("board")[id]){
+				return true;
+			}
+		},
 
 		updateState: function(id) {
-			// your code here
+			if (this.hasMark(id)){
+				return false;
+			} else {
+				var board = this.get("board")
+				board[id] = this.player();
+				this.set("board", board);
+				this.trigger(this.player(), id);
+			}
+			return true;
 		},
 
 		tie: function() {
-			// your code here
+			for (var i = 0; i < this.get("board").length; i++) {
+				if (!this.hasMark(i)) {
+					return false;
+				}
+			}
+			return true;
+		},
+
+		checkFor3: function(combo) {
+			for(var i = 0; i < combo.length; i++) {
+				if (this.get("board")[combo[i]] != this.player()) {
+					return false;
+				}
+			}
+			return true;
 		},
 		
 		gameOver: function() {
-			// your code here
+			if (this.winnerCheck()) {
+				this.set("endStatus", "win");
+				return true;
+			} else if (this.tie()) {
+				this.set("endStatus", "tie");
+				return true;
+			} else {
+				return false;
+			}
 		},
 		
 		player: function() {
-			// your code here
+			if (this.get("turn") % 2 == 0) {
+				return "X";
+			} else {
+				return "O";
+			}
 		},
 		
 		winnerCheck: function() {
-			// your code here
+			for(var i = 0; i < this.get("winningCombos").length; i++) {
+				if (this.checkFor3(this.get("winningCombos")[i]) == true) {
+					return true;
+				}
+			}
+			return false;
+		},
+
+		addTurn: function(){
+			var turn = this.get("turn");
+			this.set("turn", turn + 1);
 		},
 		
 		doTurn: function(id) {
-			// your code here
+			
+			// Add mark to virtual board
+			if (this.updateState(id)){
+				
+				// Check for winner
+				if (this.gameOver()) {
+					// Alert for winner (view)
+					this.trigger(this.get("endStatus"), this.player());
+				}
+
+				// Increment turn
+				this.addTurn();
+			}
 		}
 		
 	})
